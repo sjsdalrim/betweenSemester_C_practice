bbc2371553b7099fff67d5f35fc5d757bed44afb
Junsang Seo <72577353+sjsdalrim@users.noreply.github.com>
2021-06-25 16:01:18 +0900

    Chapter 01

---
+++ ch01_compile&execute.c
@@ -0,0 +1,17 @@
+/*
+家胶内靛 Source code (.c)
+
+-> 哪颇老 Compile
+格利 颇老 Object file (.obj)
+ 
+-> 傅农&肺歹 Link&Loader
+角青 颇老 Execution file (.exe)
+
+*/
+
+#include <stdio.h>
+
+main() {
+	printf("Hello World!");
+}
+
